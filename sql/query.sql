a84d7d70d07fff2e7615c0b2701c0a2238968445
kimyongsu <akys159357@naver.com>
2024-05-11 18:50:10 +0900

    충돌 해결

---
+++ sql/query.sql
@@ -0,0 +1,8 @@
+INSERT INTO ssafytrip.tour_content (name, kor_name) VALUES ('TOURIST_SPOT', '관광지');
+INSERT INTO ssafytrip.tour_content (name, kor_name) VALUES ('STAY', '숙소');
+INSERT INTO ssafytrip.tour_content (name, kor_name) VALUES ('RESTAURANT', '식당');
+INSERT INTO ssafytrip.tour_content (name, kor_name) VALUES ('CULTURE', '문화시설');
+INSERT INTO ssafytrip.tour_content (name, kor_name) VALUES ('SHOW', '공연');
+INSERT INTO ssafytrip.tour_content (name, kor_name) VALUES ('TRAVEL', '여행');
+INSERT INTO ssafytrip.tour_content (name, kor_name) VALUES ('SHOPPING', '쇼핑');
+INSERT INTO ssafytrip.tour_content (name, kor_name) VALUES ('LEISURE', '레저');
