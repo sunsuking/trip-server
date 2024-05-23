e1aaa364b2e4e58232c94c0eab3609d20c14cb48
sunsuking <sunsuking@gmail.com>
2024-05-23 15:40:18 +0900

    에러 처리 추가

--- src/main/java/com/ssafy/trip/core/advice/CustomControllerAdvice.java
+++ src/main/java/com/ssafy/trip/core/advice/CustomControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.ssafy.trip.core.advice;
 
 import com.ssafy.trip.core.exception.CustomException;
+import com.ssafy.trip.core.exception.ErrorCode;
 import com.ssafy.trip.core.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLException;
+
 @Slf4j
 @RestControllerAdvice
 public class CustomControllerAdvice {
@@ -18,14 +21,20 @@ public class CustomControllerAdvice {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ResponseBody
     public ErrorResponse handleException(Exception exception) {
-        exception.printStackTrace();
         log.error("Exception: {}", exception.getMessage());
         return ErrorResponse.of(exception);
     }
 
+    @ExceptionHandler(SQLException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ErrorResponse handleSQLException(SQLException exception) {
+        log.error("SQLException: {}", exception.getMessage());
+        return ErrorResponse.of(new CustomException(ErrorCode.SERVER_ERROR));
+    }
+
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> handleCustomException(CustomException exception) {
-//        exception.printStackTrace();
         log.error("CustomException: {}", exception.getMessage());
         return ResponseEntity.status(exception.getErrorCode().getStatus())
                 .body(ErrorResponse.of(exception));
